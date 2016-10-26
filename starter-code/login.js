2c3e1bb810647164828eb7cd752c9fe5d4c30196
virginiachae <chae.virginia@gmail.com>
2016-10-26 12:27:54 -0700

    finished login.js

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,16 @@
 console.log("login.js loaded");
+var userLogin = {name: 'virginia',
+                  password: 'hello'};
+
+var userpw = prompt('Enter password for user '+ userLogin.name)
+var numIterations = 0;
+
+if (userpw === userLogin.password){
+  console.log('password is correct');
+}
+
+while (userpw !== userLogin.password && numIterations < 2){
+  window.alert('The password is incorrect');
+  userpw = prompt('Enter password for user '+ userLogin.name);
+  numIterations++;
+}
