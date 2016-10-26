1ea827ad0872260ac708ed9d2dd76b2bb57ba3d9
virginiachae <chae.virginia@gmail.com>
2016-10-26 12:05:46 -0700

    finished security_questions.js

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,17 @@
 console.log("security_questions.js loaded");
+var securityQuestions = [
+  {question: "What was the name of your first pet?",
+   expectedAnswer: "Casper"},
+  {question: "What is your mother's maiden name?",
+   expectedAnswer: "Kim"},
+  {question: "Who is your favorite sports team?",
+   expectedAnswer: "Warriors"}
+]
+
+for (i=0; i<securityQuestions.length; i++){
+  var userAnswer = prompt(securityQuestions[i].question);
+  if (securityQuestions[i].expectedAnswer !== userAnswer){
+    window.alert('You have answered incorrectly');
+    break;
+  }
+}
